d9b9f78491eda8615690285435a62e6aed0df20c
Carlos Zabala <czabala847@gmail.com>
2026-08-26 21:26:10 -0500

    feat: enhance getGifsByQuery function with error handling and empty query check

--- src/projects/02-gifsApp/gifs/actions/__tests__/get-gifs-by-query.action.test.ts
+++ src/projects/02-gifsApp/gifs/actions/__tests__/get-gifs-by-query.action.test.ts
@@ -1,30 +1,34 @@
 import AxiosMockAdapter from "axios-mock-adapter"
-import { describe, expect, test } from "vitest"
+import { beforeEach, describe, expect, test, vi } from "vitest"
 import { giphyApi } from "../../api/giphy.api"
 import { getGifsByQuery } from "../get-gifs-by-query.action"
 
 describe("get-gifs-by-query.action", () => {
-  const mock = new AxiosMockAdapter(giphyApi);
+  let mock = new AxiosMockAdapter(giphyApi);
 
-  mock.onGet("/search").reply(200, {
-    data: [
-      {
-        id: "123",
-        title: "title",
-        images: {
-          original: {
-            url: "url",
-            width: "100",
-            height: "100",
-          },
-        },
-      },
-    ],
+  beforeEach(() => {
+    mock = new AxiosMockAdapter(giphyApi);
   });
 
   test("should return a list of gifs", async () => {
+    mock.onGet("/search").reply(200, {
+      data: [
+        {
+          id: "123",
+          title: "title",
+          images: {
+            original: {
+              url: "url",
+              width: "100",
+              height: "100",
+            },
+          },
+        },
+      ],
+    });
+
     const gifs = await getGifsByQuery("test");
-    expect(gifs).toHaveLength(1)
+    expect(gifs).toHaveLength(1);
 
     //others validations
     expect(gifs[0].id).toBe("123");
@@ -32,5 +36,28 @@ describe("get-gifs-by-query.action", () => {
     expect(gifs[0].url).toBe("url");
     expect(gifs[0].width).toBe(100);
     expect(gifs[0].height).toBe(100);
+  });
+
+  test("should return a empty list if query is empty", async () => {
+    mock.restore();
+    const gifs = await getGifsByQuery("");
+    expect(gifs).toHaveLength(0);
+  });
+
+  test("should handle error when the API fails", async () => {
+    const consoleErrorSpy = vi
+      .spyOn(console, "error")
+      .mockImplementation(() => {});
+
+    mock.onGet("/search").reply(400, {
+      data: {
+        message: "bad request",
+      },
+    });
+
+    const gifs = await getGifsByQuery("test");
+    expect(gifs).toHaveLength(0);
+    expect(consoleErrorSpy).toHaveBeenCalled();
+    expect(consoleErrorSpy).toHaveBeenCalledWith(expect.anything());
   });
 });
